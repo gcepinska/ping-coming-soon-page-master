9b22229a44e8c51788f1a67838b79680c020a614
gcepinska <gcepinska@gmail.com>
2022-07-31 17:44:34 +0200

    added queries

--- script.js
+++ script.js
@@ -4,27 +4,7 @@ const inputEmail = document.getElementById("email");
 const emailError = document.querySelector(".email-error");
 const btnNotify = document.querySelector(".btn");
 
-// const validEmail = function () {
-//   const re = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-//   const html = `<p class="email-error hidden"><i>Please provide valid email adress</i></p>`;
-
-//   if (!re.test(inputEmail.value)) {
-//     // inputEmail.innerHTML = "";
-//     inputEmail.insertAdjacentHTML("afterend", html);
-//     inputEmail.style.borderColor = "red";
-//     inputEmail.focus();
-//     emailError.classList.remove("hidden");
-//     return false;
-//   } else {
-//     // inputEmail.innerHTML="";
-//     emailError.classList.add("hidden");
-//     inputEmail.style.borderColor = "#d0daf3";
-//     inputEmail.focus();
-//     return true;
-//   }
-// };
-
-/////// Version 1 
+///// Version 1
 const validEmail = function () {
   const re = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   if (!re.test(inputEmail.value)) {
@@ -39,6 +19,27 @@ const validEmail = function () {
     return true;
   }
 };
+
+/// Version 2
+
+// const validEmail = function () {
+//   const re = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+//   const html = `<p class="email-error"><i>Please provide valid email adress</i></p>`;
+//   const emailError = document.querySelector(".email-error");
+  
+//   if (!re.test(inputEmail.value)) {
+//     inputEmail.insertAdjacentHTML("afterend", html);
+//     emailError.innerHTML = "";
+//     inputEmail.style.borderColor = "red";
+//     inputEmail.focus();
+//     return false;
+//   } else {
+//     emailError.innerHTML = "";
+//     inputEmail.style.borderColor = "#d0daf3";
+//     inputEmail.focus();
+//     return true;
+//   }
+// };
 
 btnNotify.addEventListener("click", validEmail);
 
